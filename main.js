059e39acbcb3cbaacae5be7afb7a8410de397322
Andrey Mitkin <am@letsnova.ru>
2019-07-12 15:52:47 +0300

    fix

--- main.js
+++ main.js
@@ -3,21 +3,23 @@
 // browser.get('https://nova.dev.aetalon.tech');
 // browser.quit();
 const {Builder, By, Key, until} = require('selenium-webdriver');
-async function browse() {
+function browse() {
     let driver = new Builder().forBrowser('chrome').build();
     // driver.manage().setTimeouts(10000);
     try {
         driver.get('https://nova.dev.aetalon.tech');
         let elements = driver.wait(
-            until.elementsLocated(By.tagName('div')),
+            until.elementLocated(By.className('my-0')),
             20000
             );
         // await elements;
         let element = driver.getTitle();
-        console.log(element);
+        console.log(elements.getId());
+        driver.sleep(5000);
+        driver.quit();
     }
     catch(err) {
         console.log('something going wrong: \n' + err.toString());
     }
 };
-browse();
+// browse();
