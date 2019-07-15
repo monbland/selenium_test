f42f8fcc012764cc9195ad2323aeb34210d5d184
Andrey Mitkin <am@letsnova.ru>
2019-07-15 13:06:29 +0300

    #fix

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-// var webdriver = require('selenium-webdriver');
-// var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'safari'}).build();
-// browser.get('https://nova.dev.aetalon.tech');
-// browser.quit();
 const {Builder, By, Key, until} = require('selenium-webdriver');
-function browse() {
-    let driver = new Builder().forBrowser('chrome').build();
-    // driver.manage().setTimeouts(10000);
+let driver = new Builder().forBrowser('chrome').build();
+async function clickLoadBack(button){
+    await button.click();
+    // await driver.navigate().back();
+}
+async function example() {
     try {
+        let time = new Date();
         driver.get('https://nova.dev.aetalon.tech');
-        let elements = driver.wait(
-            until.elementLocated(By.className('my-0')),
-            20000
-            );
-        // await elements;
-        var element = driver.getTitle();
-        console.log(elements.getId());
-        driver.sleep(5000);
-        driver.quit();
+        let elem = await driver.wait(until.elementLocated(By.className('py-0')));
+        await elem.click();
+        let inputForm = await driver.wait(until.elementLocated(By.className('form-control')));
+        inputForm = await driver.findElements(By.className('form-control'));
+        await inputForm[0].sendKeys('Viktor');
+        await inputForm[1].sendKeys('7188387q');
+        let submitButton = await driver.findElement(By.className('btn-warning')).click();
+        let navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/events']")));
+        await clickLoadBack(navigationItem);
+        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/persons']")));
+        await clickLoadBack(navigationItem);
+        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/devices']")));
+        await clickLoadBack(navigationItem);
+        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/admin/measurements/lab-form']")));
+        await clickLoadBack(navigationItem);
+        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/tags']")));
+        await clickLoadBack(navigationItem);
+        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/test-tags']")));
+        await clickLoadBack(navigationItem);
+        // let elem2 = await driver.wait(until.titleIs('nova'));
+        let resultTime = new Date() - time;
+        console.log("loading all tabs complete in " + resultTime + " ms");
+        await driver.quit();
+
+    } catch (err) {
+        console.log('something is wrong: ' + err.toString());
+    }
+}
+for (var i = 0; i < 1; i++) {
+    try {
+        example();
     }
     catch(err) {
-        console.log('something going wrong: \n' + err.toString());
+        console.log('something is wrong: ' + err.toString());
     }
-};
-browse();
+}
