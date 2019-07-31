6d809319cba5a3aa5eaf2ff57cc526d5125bee4b
Andrey Mitkin <am@letsnova.ru>
2019-07-31 15:56:01 +0300

    Переработал структуру, считает после каждой подгрузки страницы, начал делать поиск по элементам на самой странице через xpath

--- main.js
+++ main.js
@@ -11,27 +11,71 @@ async function example() {
         let elem = await driver.wait(until.elementLocated(By.className('py-0')));
         await elem.click();
         let inputForm = await driver.wait(until.elementLocated(By.className('form-control')));
+        let loginFormLoad = new Date() - time;
         inputForm = await driver.findElements(By.className('form-control'));
         await inputForm[0].sendKeys('Viktor');
         await inputForm[1].sendKeys('7188387q');
         let submitButton = await driver.findElement(By.className('btn-warning')).click();
-        let navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/events']")));
-        await clickLoadBack(navigationItem);
-        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/persons']")));
-        await clickLoadBack(navigationItem);
-        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/devices']")));
-        await clickLoadBack(navigationItem);
-        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/admin/measurements/lab-form']")));
-        await clickLoadBack(navigationItem);
-        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/tags']")));
-        await clickLoadBack(navigationItem);
-        navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/test-tags']")));
-        await clickLoadBack(navigationItem);
-        // let elem2 = await driver.wait(until.titleIs('nova'));
-        let resultTime = new Date() - time;
-        console.log("loading all tabs complete in " + resultTime + " ms");
-        await driver.quit();
 
+        let navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/persons']")));
+        let loginCompleteTime = new Date() - time;
+        await clickLoadBack(navigationItem);
+
+        navigationItem = await driver.wait(until.elementsLocated(By.xpath("//div[@class='content']/div/div/div[1]/div[1]/div/div/p[1]")));
+        await setTimeout(() => {
+            let personsCount = driver.findElement(By.xpath("//div[@class='content']/div/div/div[1]/div[1]/div/div/p[1]"));
+            personsCount.getText().then(
+                result => {
+                    console.log(result.toString())
+                },
+                error => {
+                    console.log(error.message)
+                }
+            )
+        }, 2000);
+        // navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/health']")));
+        // let personsLoad = new Date() - time;
+        // await clickLoadBack(navigationItem);
+        //
+        // navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/activity']")));
+        // let healthLoad = new Date() - time;
+        // await clickLoadBack(navigationItem);
+        //
+        // navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/events']")));
+        // let activityLoad = new Date() - time;
+        // await clickLoadBack(navigationItem);
+        //
+        // navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/devices']")));
+        // let eventsLoad = new Date() - time;
+        // await clickLoadBack(navigationItem);
+        //
+        // navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/admin/measurements/lab-form']")));
+        // let devicesLoad = new Date() - time;
+        // await clickLoadBack(navigationItem);
+        //
+        // navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/tags']")));
+        // let measurementsLoad = new Date() - time;
+        // await clickLoadBack(navigationItem);
+        //
+        // navigationItem = await driver.wait(until.elementLocated(By.xpath("//a[@href='/test-tags']")));
+        // let tagsLoad = new Date() - time;
+        // await clickLoadBack(navigationItem);
+        // // let elem2 = await driver.wait(until.titleIs('nova'));
+        // let resultTime = new Date() - time;
+        // console.log("loading: \n" +
+        //     "login          - " + loginFormLoad + " ms\n" +
+        //     "login complete - " + loginCompleteTime + " ms\n" +
+        //     "persons        - " + personsLoad + " ms\n" +
+        //     "health         - " + healthLoad + " ms\n" +
+        //     "activity       - " + activityLoad + " ms\n" +
+        //     "events         - " + eventsLoad + " ms\n" +
+        //     "devices        - " + devicesLoad + " ms\n" +
+        //     "measurements   - " + measurementsLoad + " ms\n" +
+        //     "tags           - " + tagsLoad + " ms\n" +
+        //     "all tabs complete in " + resultTime + " ms");
+        await setTimeout(() => {
+            driver.quit();
+        }, 10000);
     } catch (err) {
         console.log('something is wrong: ' + err.toString());
     }
