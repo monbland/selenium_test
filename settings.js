d308de6a55b3b53c2a3c29d84caf988d57425df8
Andrey Mitkin <am@letsnova.ru>
2019-09-03 14:49:20 +0300

    settings fix

--- settings.js
+++ settings.js
@@ -5,4 +5,4 @@ exports.iterations = 40;
 exports.logpath = "./log.txt";
 exports.elasticurl = 'http://87.251.81.76:9200';
 exports.elasticindex = 'test';
-exports.chromepath = '/opt/google/chrome/chrome';
+exports.chromepath = './chromedriver';
